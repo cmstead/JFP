39769e18c633ddbb99471f309303bfd7eb407521
Chris Stead <cmstead@gmail.com>
2014-12-01 23:10:56 -0800

    Added cons, identity, maybe and either.

--- scripts/src/modules/core.js
+++ scripts/src/modules/core.js
@@ -1,10 +1,38 @@
 (function(j){
     'use strict';
 
-    function either(defaultValue, optionValue){
-        return (!!optionValue) ? optionValue : defaultValue;
+    function postpend(result, source){
+        source.forEach(function(value){
+            result.push(value);
+        });
+
+        return result;
     }
 
-    j.either = either;
+    function cons(value, source){
+        var baseArray = [];
 
+        if(value !== null && value !== undefined){
+            baseArray.push(value);
+        }
+
+        return (!!source && source.length) ? postpend(baseArray, source) : baseArray;
+    }
+
+    function identity(value){
+        return value;
+    }
+
+    function maybe(defaultValue, userFn, optionValue){
+        return (!!optionValue) ? userFn(optionValue) : defaultValue;
+    }
+
+    function either(defaultValue, optionValue){
+        return maybe(defaultValue, identity, optionValue);
+    }
+
+    j.cons = cons;
+    j.either = either;
+    j.identity = identity;
+    j.maybe = maybe;
 })(jfp);
--- spec/modules/core.spec.js
+++ spec/modules/core.spec.js
@@ -1,14 +1,65 @@
 (function(){
     'use strict';
 
-    describe('either', function(){
+    describe('identity', function(){
 
-        it('should return option value if not falsey', function(){
-            expect(j.either('default', 'option')).toBe('option');
+        it('should return a passed object', function(){
+            var obj = { test: 'test' };
+            expect(j.identity(obj)).toBe(obj);
         });
 
-        it('should return default value if option is falsey', function(){
-            expect(j.either('default', false)).toBe('default');
+    });
+
+    describe('maybe', function(){
+
+        it('should return the default value if passed value is falsey', function(){
+            var defaultValue = { test: 'test' };
+            expect(j.maybe(defaultValue, j.identity, null)).toBe(defaultValue);
+        });
+
+        it('should return passed value if it is truey and function is identity', function(){
+            var testObj = { test: 'test' };
+            expect(j.maybe('default', j.identity, testObj)).toBe(testObj);
+        });
+
+        it('should call passed function with passed object', function(){
+            var testObj = { test: 'test' },
+                spy = jasmine.createSpy('userFn');
+            j.maybe('test', spy, testObj);
+            expect(spy).toHaveBeenCalledWith(testObj);
+        });
+
+    });
+
+    describe('either', function(){
+
+        it('should return the default value if the passed value is falsey', function(){
+            var defaultValue = { test: 'test' };
+            expect(j.either(defaultValue, null)).toBe(defaultValue);
+        });
+
+        it('should return passed value if it is truey', function(){
+            var testValue = { test: 'test' };
+            expect(j.either('test', testValue)).toBe(testValue);
+        });
+
+    });
+
+    describe('cons', function(){
+
+        it('should return an empty array when no arguments are passed', function(){
+            expect(JSON.stringify(j.cons())).toBe('[]');
+        });
+
+        it('should return an array of one element if only a single element is passed', function(){
+            expect(JSON.stringify(j.cons(5))).toBe('[5]');
+        });
+
+        it('should return an array containing the first argument and the rest of a second array', function(){
+            var testArray = [1, 2, 3, 4],
+                returnedArray = j.cons(5, testArray);
+
+            expect(JSON.stringify(returnedArray)).toBe('[5,1,2,3,4]');
         });
 
     });
