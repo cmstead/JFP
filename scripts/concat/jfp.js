c93e2829efc3da0290cc9a03cda53911455f3636
Chris Stead <cmstead@gmail.com>
2014-12-24 02:37:10 +0000

    Created branch build.

--- scripts/concat/jfp.js
+++ scripts/concat/jfp.js
@@ -2,7 +2,6 @@ jfp = (function(){
     'use strict';
 
     return {};
-
 })();
 
 (function(j){
@@ -170,6 +169,29 @@ jfp = (function(){
         return !value;
     }
     
+    //Performs 'and' operation on valueSet
+    function ander(recur, current, valueSet){
+        return (valueSet.length === 0) ? 
+                current : 
+                recur(current && !!j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function and(){
+        return j.recur(ander, true, j.slice(0, arguments));
+    }
+    
+    //Performs 'or' operation on valueSet
+    function orer(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current || !!j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function or(){
+        return j.recur(orer, false, j.slice(0, arguments));
+    }
+    
+    j.and = and;
     j.isArray = isArray;
     j.isBoolean = isBoolean;
     j.isNull = isNull;
@@ -180,6 +202,7 @@ jfp = (function(){
     j.isTruthy = isTruthy;
     j.isUndefined = isUndefined;
     j.not = not;
+    j.or = or;
     
 })(jfp);
 
@@ -392,6 +415,114 @@ jfp = (function(){
     j.pluck = pluck;
     j.pluckKeys = pluckKeys;
 
+})(jfp);
+
+(function(j){
+    'use strict';
+    
+    //This is a recursive add fn
+    function adder(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current + j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function add(){
+        return j.recur(adder, 0, j.slice(0, arguments));
+    }
+    
+    //This is a recursive divide fn
+    function divider(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current / j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function divide(){
+        var args = j.slice(0, arguments),
+            first = args.length ? j.first(args) : 1;
+        return j.recur(divider, first, j.rest(args));
+    }
+    
+    //This is a recursive multiply fn
+    function multiplier(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current * j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function multiply(){
+        return j.recur(multiplier, 1, j.slice(0, arguments));
+    }
+    
+    //This is a recursive subtraction fn
+    function subtractor(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current - j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function subtract(){
+        var args = j.slice(0, arguments),
+            first = args.length ? j.first(args) : 0;
+        return j.recur(subtractor, first, j.rest(args));
+    }
+    
+    function equal(a, b){
+        var isNotUndefined = j.compose(j.not, j.isUndefined);
+        
+        return (j.and(isNotUndefined(a), 
+                      isNotUndefined(b))) ? a === b : false;
+    }
+    
+    function greater(a, b){
+        if(j.isUndefined(a) || j.isUndefined(b)){
+            throw new TypeError('Greater requires two variables for comparison');
+        }
+        
+        return a > b;
+    }
+    
+    function less(a, b){
+        if(j.isUndefined(a) || j.isUndefined(b)){
+            throw new TypeError('Less requires two variables for comparison');
+        }
+        
+        return a < b;
+    }
+    
+    function leq(a, b){
+        return less(a, b) || equal(a, b);
+    }
+    
+    function geq(a, b){
+        return greater(a, b) || equal(a, b);
+    }
+
+    //This is a recursive constructor function for ranges
+    function rangeBuilder(recur, currentRange, m, n){
+        currentRange = (m <= n) ? j.conj(m, currentRange) : currentRange;
+        return (++m <= n) ? recur(currentRange, m ,n) : currentRange;
+    }
+    
+    function range(a, b){
+        return j.recur(rangeBuilder,
+                       j.conj(a),
+                       j.either(0, a) + 1,
+                       j.either(0, b));
+    }
+
+    j.add = add;
+    j.divide = divide;
+    j.equal = equal;
+    j.geq = geq;
+    j.greater = greater;
+    j.leq = leq;
+    j.less = less;
+    j.multiply = multiply;
+    j.range = range;
+    j.subtract = subtract;
+    
 })(jfp);
 
 (function(j){
