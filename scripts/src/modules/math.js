11c5f3430a424e160723b4840ce47c0909efe3f8
Chris Stead <cmstead@gmail.com>
2014-12-24 02:25:12 +0000

    Added equal, less, greater, leq, geq, add, subtract, multiply and divide

--- scripts/src/modules/math.js
+++ scripts/src/modules/math.js
@@ -1,6 +1,86 @@
 (function(j){
     'use strict';
     
+    //This is a recursive add fn
+    function adder(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current + j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function add(){
+        return j.recur(adder, 0, j.slice(0, arguments));
+    }
+    
+    //This is a recursive divide fn
+    function divider(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current / j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function divide(){
+        var args = j.slice(0, arguments),
+            first = args.length ? j.first(args) : 1;
+        return j.recur(divider, first, j.rest(args));
+    }
+    
+    //This is a recursive multiply fn
+    function multiplier(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current * j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function multiply(){
+        return j.recur(multiplier, 1, j.slice(0, arguments));
+    }
+    
+    //This is a recursive subtraction fn
+    function subtractor(recur, current, valueSet){
+        return (valueSet.length === 0) ?
+                current :
+                recur(current - j.first(valueSet), j.rest(valueSet));
+    }
+    
+    function subtract(){
+        var args = j.slice(0, arguments),
+            first = args.length ? j.first(args) : 0;
+        return j.recur(subtractor, first, j.rest(args));
+    }
+    
+    function equal(a, b){
+        var isNotUndefined = j.compose(j.not, j.isUndefined);
+        
+        return (j.and(isNotUndefined(a), 
+                      isNotUndefined(b))) ? a === b : false;
+    }
+    
+    function greater(a, b){
+        if(j.isUndefined(a) || j.isUndefined(b)){
+            throw new TypeError('Greater requires two variables for comparison');
+        }
+        
+        return a > b;
+    }
+    
+    function less(a, b){
+        if(j.isUndefined(a) || j.isUndefined(b)){
+            throw new TypeError('Less requires two variables for comparison');
+        }
+        
+        return a < b;
+    }
+    
+    function leq(a, b){
+        return less(a, b) || equal(a, b);
+    }
+    
+    function geq(a, b){
+        return greater(a, b) || equal(a, b);
+    }
+
+    //This is a recursive constructor function for ranges
     function rangeBuilder(recur, currentRange, m, n){
         currentRange = (m <= n) ? j.conj(m, currentRange) : currentRange;
         return (++m <= n) ? recur(currentRange, m ,n) : currentRange;
@@ -12,14 +92,16 @@
                        j.either(0, a) + 1,
                        j.either(0, b));
     }
-    
-    function equal(a, b){
-        var aIsValue = j.not(j.isNull(a) || j.isUndefined(a)),
-            bIsValue = j.not(j.isNull(b) || j.isUndefined(b));
-        return (aIsValue && bIsValue) ? a === b : false;
-    }
-    
+
+    j.add = add;
+    j.divide = divide;
     j.equal = equal;
+    j.geq = geq;
+    j.greater = greater;
+    j.leq = leq;
+    j.less = less;
+    j.multiply = multiply;
     j.range = range;
+    j.subtract = subtract;
     
 })(jfp);
--- spec/modules/math.spec.js
+++ spec/modules/math.spec.js
@@ -34,6 +34,158 @@
             
         });
         
+        describe('less', function(){
+            
+            it('should throw if two values are not provided', function(){
+                function testLess(){
+                    j.less();
+                }
+                
+                expect(testLess).toThrow();
+            });
+            
+            it('should return true if first value is less than second', function(){
+                expect(j.less(1, 2)).toBe(true);
+            });
+            
+            it('should return false if first value is greater than or equal to second', function(){
+                expect(j.less(2, 1)).toBe(false);
+            });
+            
+        });
+        
+        describe('greater', function(){
+            
+            it('should throw if two values are not provided', function(){
+                function testGreater(){
+                    j.greater();
+                }
+                
+                expect(testGreater).toThrow();
+            });
+            
+            it('should return true if first value is greater than second', function(){
+                expect(j.greater(2, 1)).toBe(true);
+            });
+            
+            it('should return false if first value is less than second', function(){
+                expect(j.greater(1, 2)).toBe(false);
+            });
+            
+        });
+        
+        describe('leq', function(){
+            
+            it('should return true if values satisfy less', function(){
+                expect(j.leq(1, 2)).toBe(true);
+            });
+            
+            it('should return false if values satisfy greater', function(){
+                expect(j.leq(2, 1)).toBe(false);
+            });
+            
+            it('should return true if values satisfy equal', function(){
+                expect(j.leq(1, 1)).toBe(true);
+            });
+            
+        });
+        
+        describe('geq', function(){
+            
+            it('should return true if values satisfy greater', function(){
+                expect(j.geq(2, 1)).toBe(true);
+            });
+            
+            it('should return false if values satisfy less', function(){
+                expect(j.geq(1, 2)).toBe(false);
+            });
+            
+            it('should return true if values satisfy equal', function(){
+                expect(j.geq(1, 1)).toBe(true);
+            });
+            
+        });
+        
+        describe('add', function(){
+            
+            it('should return 0 if no values are passed', function(){
+                expect(j.add()).toBe(0);
+            });
+            
+            it('should return passed single value', function(){
+                expect(j.add(1)).toBe(1);
+            });
+            
+            it('should return sum of two values', function(){
+                expect(j.add(1, 2)).toBe(3);
+            });
+            
+            it('should the sum of multiple values', function(){
+                expect(j.add(1, 2, 3, 4, 5)).toBe(15);
+            });
+            
+        });
+        
+        describe('subtract', function(){
+            
+            it('should return 0 if no values are passed', function(){
+                expect(j.subtract()).toBe(0);
+            });
+            
+            it('should return passed single value', function(){
+                expect(j.subtract(1)).toBe(1);
+            });
+            
+            it('should return sum of two values', function(){
+                expect(j.subtract(1, 2)).toBe(-1);
+            });
+            
+            it('should the sum of multiple values', function(){
+                expect(j.subtract(1, 2, 3, 4, 5)).toBe(-13);
+            });
+            
+        });
+        
+        describe('multiply', function(){
+            
+            it('should return 0 if no values are passed', function(){
+                expect(j.multiply()).toBe(1);
+            });
+            
+            it('should return passed single value', function(){
+                expect(j.multiply(2)).toBe(2);
+            });
+            
+            it('should return product of two values', function(){
+                expect(j.multiply(2, 3)).toBe(6);
+            });
+            
+            it('should the product of multiple values', function(){
+                expect(j.multiply(2, 3, 4, 5)).toBe(120);
+            });
+            
+        });
+
+        describe('divide', function(){
+            
+            it('should return 0 if no values are passed', function(){
+                expect(j.divide()).toBe(1);
+            });
+            
+            it('should return passed single value', function(){
+                expect(j.divide(2)).toBe(2);
+            });
+            
+            it('should return 1/product of two values', function(){
+                expect(j.divide(6, 3)).toBe(2);
+            });
+            
+            it('should the 1/product of multiple values', function(){
+                expect(j.divide(12, 3, 2, 2)).toBe(1);
+            });
+            
+        });
+
     });
     
 })();
