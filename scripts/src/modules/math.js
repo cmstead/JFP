efa3edc07af9fbe20d880ff29bfdc5be67a6e429
Chris Stead <cmstead@gmail.com>
2014-12-24 00:45:13 +0000

    Added equality function

--- scripts/src/modules/math.js
+++ scripts/src/modules/math.js
@@ -13,6 +13,13 @@
                        j.either(0, b));
     }
     
+    function equal(a, b){
+        var aIsValue = j.not(j.isNull(a) || j.isUndefined(a)),
+            bIsValue = j.not(j.isNull(b) || j.isUndefined(b));
+        return (aIsValue && bIsValue) ? a === b : false;
+    }
+    
+    j.equal = equal;
     j.range = range;
     
 })(jfp);
--- spec/modules/math.spec.js
+++ spec/modules/math.spec.js
@@ -18,6 +18,22 @@
             });
         });
         
+        describe('equal', function(){
+            
+            it('should return falseif less than two values provided', function(){
+                expect(j.equal()).toBe(false);
+            });
+            
+            it('should return true if both values are equal', function(){
+                expect(j.equal(0, 0)).toBe(true);
+            });
+            
+            it('should return false if both values are not equal', function(){
+                expect(j.equal(0, 1)).toBe(false);
+            });
+            
+        });
+        
     });
     
 })();
