43c2e7f54b9b4019d17f8c18e9a768a1aed76ced
Chris Stead <cmstead@gmail.com>
2015-01-13 12:58:26 -0800

    Work toward creating a npm build and better distribution.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 var cleanConfig = require('./grunt/clean.json'),
     concatConfig = require('./grunt/concat.json'),
+    copyConfig = require('./grunt/copy.json'),
     jsdocConfig = require('./grunt/jsdoc.json'),
     jshintConfig = require('./grunt/jshint.json'),
     karmaConfig = require('./grunt/karma.json'),
@@ -11,6 +12,7 @@ module.exports = function(grunt){
 
         clean: cleanConfig,
         concat: concatConfig,
+        copy: copyConfig,
         jsdoc : jsdocConfig,
         jshint: jshintConfig,
         karma: karmaConfig,
@@ -21,6 +23,7 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-jsdoc');
