de910e578f10469d49816d22fdf123fa898e8159
Chris Stead <cmstead@gmail.com>
2015-05-28 23:22:43 -0700

    Started creating definition TypeScript file for JFP v2.

---
+++ jfp.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for JFP
+// Project: http://cmstead.github.io/JFP
+// Definitions by: Chris Stead <http://www.chrisstead.com>
+
+declare var j: j.jfpStatic
+
+declare module j{
+	interface jfpStatic{}
+}
