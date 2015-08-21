62e738151b9a79ea4f333a2536fe2dd45d41a31b
Chris Stead <cmstead@gmail.com>
2015-08-20 23:49:40 -0700

    Removed watch from gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,7 @@ var cleanConfig = require('./grunt/clean.json'),
     copyConfig = require('./grunt/copy.json'),
     jasmine = require('./grunt/jasmine.json'),
     jshintConfig = require('./grunt/jshint.json'),
-    uglifyConfig = require('./grunt/uglify.json'),
-    watch = require('./grunt/watch.json');
+    uglifyConfig = require('./grunt/uglify.json');
 
 module.exports = function(grunt){
     grunt.initConfig({
@@ -15,8 +14,7 @@ module.exports = function(grunt){
         copy: copyConfig,
         jasmine_nodejs: jasmine,
         jshint: jshintConfig,
-        uglify: uglifyConfig,
-        watch: watch
+        uglify: uglifyConfig
     });
 
     /* Load grunt task adapters */
@@ -26,14 +24,12 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-jasmine-nodejs');
 
     /* Register composite grunt tasks */
 
     grunt.registerTask('test', ['clean', 'concat', 'jshint', 'jasmine_nodejs']);
     grunt.registerTask('build', ['clean', 'concat', 'jshint', 'jasmine_nodejs', 'uglify']);
-    grunt.registerTask('build-watch', ['build', 'watch']);
 
     grunt.registerTask('default', ['build']);
 };
