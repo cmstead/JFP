bef0157313ab2ce0dc4946c1c6b0f11cfbf4d15f
Chris Stead <cmstead@gmail.com>
2016-06-01 00:03:48 -0700

    Added range and mod functions

--- scripts/src/modules/math.js
+++ scripts/src/modules/math.js
@@ -19,15 +19,33 @@
         return a / b;
     }
     
+    function mod (a, b){
+        return a % b;
+    }
+    
     function curryOperation (operation) {
         return function (a, b) {
-            return isUndefined(b) ? j.partial(operation, a) : operation(a, b);
+            return isUndefined(b) ? j.rpartial(operation, a) : operation(a, b);
         };
     }
     
+    function range (a, b, interval){
+        var min = isUndefined(b) ? 1 : a;
+        var max = isUndefined(b) ? a : b;
+        var offset = isUndefined(interval) ? 1 : interval;
+        
+        return j.recur(buildRange)(min, []);
+        
+        function buildRange (recur, value, output){
+            return value > max ? output : recur(value + offset, j.concat(output, [value]));
+        }
+    }
+    
     j.add = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(add));
-    j.subtract = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(subtract));
-    j.multiply = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(multiply));
     j.divide = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(divide));
+    j.mod = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(mod));
+    j.multiply = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(multiply));
+    j.range = j.enforce('int, [int], [int] => array<int>', range);
+    j.subtract = j.enforce('number, [number] => taggedUnion<function;number>', curryOperation(subtract));
     
 })(jfp);
--- spec/modules/math.spec.js
+++ spec/modules/math.spec.js
@@ -4,11 +4,11 @@ describe('JFP math', function () {
     
     describe('add', function () {
         
-        it('should add two numbers, in a curried way', function () {
+        it('should add two numbers as a curried function', function () {
             expect(j.add(5)(6)).toBe(11);
         });
         
-        it('should add two numbers, in a standard way', function () {
+        it('should add two numbers with a standard call', function () {
             expect(j.add(5, 6)).toBe(11);
         });
         
@@ -16,11 +16,11 @@ describe('JFP math', function () {
     
     describe('subtract', function () {
         
-        it('should subtract two numbers, in a curried way', function () {
-            expect(j.subtract(5)(6)).toBe(-1);
+        it('should subtract first from second when curried', function () {
+            expect(j.subtract(5)(6)).toBe(1);
         });
         
-        it('should subtract two numbers, in a standard way', function () {
+        it('should subtract two numbers second when called normally', function () {
             expect(j.subtract(5, 6)).toBe(-1);
         });
         
@@ -28,11 +28,11 @@ describe('JFP math', function () {
     
     describe('multiply', function () {
         
-        it('should multiply two numbers, in a curried way', function () {
+        it('should multiply two numbers as a curried function', function () {
             expect(j.multiply(5)(6)).toBe(30);
         });
         
-        it('should multiply two numbers, in a standard way', function () {
+        it('should multiply two numbers as a standard function call', function () {
             expect(j.multiply(5, 6)).toBe(30);
         });
         
@@ -40,12 +40,40 @@ describe('JFP math', function () {
     
     describe('divide', function () {
         
-        it('should divide two numbers, in a curried way', function () {
-            expect(j.divide(6)(3)).toBe(2);
+        it('should divide second number by first when called as a curried function', function () {
+            expect(j.divide(3)(6)).toBe(2);
         });
         
-        it('should divide two numbers, in a standard way', function () {
+        it('should divide first by second when called in standard fashion', function () {
             expect(j.divide(6, 3)).toBe(2);
+        });
+        
+    });
+    
+    describe('mod', function () {
+        
+        it('should take mod of second number by first when called as a curried function', function () {
+            expect(j.mod(2)(5)).toBe(1);
+        });
+        
+        it('should take mod of first by second when called in standard fashion', function () {
+            expect(j.mod(5, 2)).toBe(1);
+        });
+        
+    });
+    
+    describe('range', function () {
+        
+        it('should produce a range from 1 to n', function () {
+            expect(JSON.stringify(j.range(5))).toBe('[1,2,3,4,5]');
+        });
+        
+        it('should produce a range from -5 to n', function () {
+            expect(JSON.stringify(j.range(-5, 5))).toBe('[-5,-4,-3,-2,-1,0,1,2,3,4,5]');
+        });
+        
+        it('should produce a range with an interval', function () {
+            expect(JSON.stringify(j.range(1, 5, 2))).toBe('[1,3,5]');
         });
         
     });
